65ff62967e8ee58ce694064fdcd4355c7c0d9949
Konstantin <pceptron@yandex.ru>
2025-01-06 21:57:33 +0300

    Add files via upload

---
+++ fill-db.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Configuration of rotation ofr netflow statistics
+# More details here: https://github.com/vmxdev/xenoeye
+EXP_DIR="/var/lib/xenoeye/exp"
+FAIL_DIR="/var/lib/xenoeye/expfailed/"
+
+for sqlscript in $EXP_DIR/*.sql; do
+  psql postgresql://<DB_USERNAME>:<PASSWORD>@<DB_HOST>/xenoeyedb -f "$sqlscript"
+  if [ $? -eq 0 ]; then
+      rm -f "$sqlscript"
+  else
+      mv "$sqlscript" $FAIL_DIR
+  fi
+done
+
